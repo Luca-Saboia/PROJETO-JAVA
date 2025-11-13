81ea271bba7b8840fdc08e3b9d509c3954127ce6
Luca-Saboia <luca.silva.saboia@gmail.com>
2025-11-13 20:56:18 +0000

    Projeto2 1.0

--- src/Main.java
+++ src/Main.java
@@ -7,23 +7,39 @@ public class Main {
     private static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        while (true) {
+        String opc;
+        do {
             System.out.println("*** MACK THEATHER ***");
             System.out.println("1) Cadastrar Espetáculo");
             System.out.println("2) Cadastrar Cliente");
             System.out.println("3) Compra de Entradas");
             System.out.println("4) Sair");
             System.out.print("Selecione uma opção: ");
-            String opc = sc.nextLine().trim();
+            pc = sc.nextLine().trim();
 
-            if (opc.equals("1")) cadastrarEspetaculo();
-            else if (opc.equals("2")) cadastrarCliente();
-            else if (opc.equals("3")) compraDeEntradas();
-            else if (opc.equals("4")) {
-                System.out.println("Saindo...");
-                return;
-            } else System.out.println("Opção inválida. Escolha entre 1 e 4.\n");
-        }
+            switch (opc) {
+                case "1":
+                    cadastrarEspetaculo();
+                    break;
+
+                case "2":
+                    cadastrarCliente();
+                    break;
+
+                case "3":
+                    compraDeEntradas();
+                    break;
+
+                case "4":
+                    System.out.println("Saindo...");
+                    break;
+
+                default:
+                    System.out.println("Opção inválida. Escolha entre 1 e 4.\n");
+                    break;
+            }
+
+        } while (!opc.equals("4"));
     }
 
     private static void cadastrarEspetaculo() {
